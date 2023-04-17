937742af964c6bd942c379b7eba4648319d419cc
Hompushparaj mehta <115880885+Pushparaj1381@users.noreply.github.com>
2023-04-17 09:23:18 +0530

    Edited

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -24,8 +24,8 @@ const Services = () => {
       {/* left side */}
       <div className="awesome">
         {/* dark mode */}
-        <span style={{ color: darkMode ? "white" : "" }}>Web development</span>
-        <span>solutions</span>
+        <span style={{ color: darkMode ? "white" : "" }}>Bringing your imaginative <br /> into reality</span>
+        <span>with code and design</span>
         <spane>
           Transforming Ideas into Engaging Digital Experiences.
           <br />
